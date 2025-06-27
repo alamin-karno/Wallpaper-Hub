a2bf7b7ad96e84eae5bad936baf19bbdea8f7764
Md. Al-Amin <amin15-1951@diu.edu.bd>
2025-06-27 16:35:26 +0600

    Wallpaper Hub Android and Flutter Updated
    This commit updates the Android and Flutter configurations and dependencies for the Wallpaper Hub application.
    
    Key changes include:
    - **Android:**
        - Updated `AndroidManifest.xml` to set `android:exported="true"` for the main activity.
        - Modified `build.gradle` (project level) to use `mavenCentral()` instead of `jcenter()` and added a subprojects configuration for namespace.
        - Updated `app/build.gradle` to:
            - Use Gradle plugins block.
            - Set `namespace`.
            - Update `compileSdkVersion` to 35 and `minSdkVersion` to 21.
            - Configure `compileOptions` and `kotlinOptions` for Java 1.8.
            - Removed explicit Kotlin dependency as it's handled by the plugin.
        - Updated `gradle-wrapper.properties` to Gradle version 8.4.
        - Updated `settings.gradle` to the new plugin management and dependency resolution format.
    - **Flutter:**
        - Updated SDK constraints and various package dependencies in `pubspec.yaml` and `pubspec.lock`.
        - Minor code cleanup in `lib/main.dart`.
        - Updated `lib/screens/image_fullview.dart` to use `WallpaperManagerPlus` and `ImageGallerySaverPlus`, and adjusted wallpaper setting logic.

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Wallpaper Hub',
-      theme: ThemeData(
-        primaryColor: Colors.white,
-      ),
+      theme: ThemeData(primaryColor: Colors.white),
       home: Home(),
     );
   }
 }
-
--- lib/screens/image_fullview.dart
+++ lib/screens/image_fullview.dart
@@ -1,10 +1,6 @@
-
 import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:carousel_slider/carousel_controller.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
@@ -13,26 +9,29 @@ import 'package:flutter/services.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:focused_menu/focused_menu.dart';
 import 'package:focused_menu/modals.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:image_gallery_saver_plus/image_gallery_saver_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:wallpaper_hub/model/wallpaper_model.dart';
-import 'package:wallpaper_manager/wallpaper_manager.dart';
+import 'package:wallpaper_manager_plus/wallpaper_manager_plus.dart';
 
 class ImageView extends StatefulWidget {
+  ImageView({
+    required this.imgUrl,
+    required this.imgUrlLarge,
+    required this.wallpapers,
+  });
 
-  final String imgUrl,imgUrlLarge;
-  List<WallpaperModel> wallpapers;
-  ImageView({required this.imgUrl,required this.imgUrlLarge,required this.wallpapers});
+  final String imgUrl, imgUrlLarge;
+  final List<WallpaperModel> wallpapers;
 
   @override
   _ImageViewState createState() => _ImageViewState();
 }
 
 class _ImageViewState extends State<ImageView> {
-
   String _wallpaper = 'Set Wallpaper';
   bool isDownloaded = false;
   bool isWallpaperSet = false;
@@ -42,16 +41,15 @@ class _ImageViewState extends State<ImageView> {
     return Scaffold(
       body: Stack(
         children: [
-
           Hero(
             tag: widget.imgUrl,
             child: Container(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width,
-                child: Image(
-                  image: CachedNetworkImageProvider(widget.imgUrl),
-                  fit: BoxFit.cover,
-                ),
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Image(
+                image: CachedNetworkImageProvider(widget.imgUrl),
+                fit: BoxFit.cover,
+              ),
               /*Builder(
                   builder: (context) {
                     final double height = MediaQuery.of(context).size.height;
@@ -73,7 +71,6 @@ class _ImageViewState extends State<ImageView> {
                 ),*/
             ),
           ),
-
           Container(
             height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
@@ -84,7 +81,7 @@ class _ImageViewState extends State<ImageView> {
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 FocusedMenuHolder(
-                  menuWidth: MediaQuery.of(context).size.width/2,
+                  menuWidth: MediaQuery.of(context).size.width / 2,
                   animateMenuItems: true,
                   blurSize: 5,
                   duration: Duration(microseconds: 100),
@@ -96,54 +93,54 @@ class _ImageViewState extends State<ImageView> {
                     FocusedMenuItem(
                         title: Text('Set on HomeScreen'),
                         onPressed: () {
-                          isWallpaperSet ? setState(() {
-                            _wallpaper = 'Already Set';
-                          }) : _setWallpaper(WallpaperManager.HOME_SCREEN);
-                        }
-                        ),
+                          isWallpaperSet
+                              ? setState(() {
+                                  _wallpaper = 'Already Set';
+                                })
+                              : _setWallpaper(WallpaperManagerPlus.homeScreen);
+                        }),
                     FocusedMenuItem(
                         title: Text('Set on LockScreen'),
                         onPressed: () {
-                          isWallpaperSet ? setState(() {
-                            _wallpaper = 'Already Set';
-                          }) : _setWallpaper(WallpaperManager.LOCK_SCREEN);
-                        }
-                    ),
+                          isWallpaperSet
+                              ? setState(() {
+                                  _wallpaper = 'Already Set';
+                                })
+                              : _setWallpaper(WallpaperManagerPlus.lockScreen);
+                        }),
                     FocusedMenuItem(
                         title: Text('Set on Both'),
                         onPressed: () {
-                          isWallpaperSet ? setState(() {
-                            _wallpaper = 'Already Set';
-                          }) : _setWallpaper(WallpaperManager.BOTH_SCREENS);
-                    }),
+                          isWallpaperSet
+                              ? setState(() {
+                                  _wallpaper = 'Already Set';
+                                })
+                              : _setWallpaper(WallpaperManagerPlus.bothScreens);
+                        }),
                   ],
                   onPressed: () {},
                   child: Stack(
                     children: [
-
                       Container(
                         height: 50,
-                        width: MediaQuery.of(context).size.width/2,
+                        width: MediaQuery.of(context).size.width / 2,
                         decoration: BoxDecoration(
                           color: Color(0xff1C1B1B).withOpacity(0.6),
                           borderRadius: BorderRadius.circular(16),
                         ),
                       ),
-
                       Container(
                         height: 50,
-                        width: MediaQuery.of(context).size.width/2,
-                        padding: EdgeInsets.symmetric(horizontal: 8,vertical: 8),
+                        width: MediaQuery.of(context).size.width / 2,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 8, vertical: 8),
                         decoration: BoxDecoration(
                             border: Border.all(color: Colors.white54, width: 1),
                             borderRadius: BorderRadius.circular(16),
-                            gradient: LinearGradient(
-                                colors: [
-                                  Color(0x36FFFFFF),
-                                  Color(0x0FFFFFFF),
-                                ]
-                            )
-                        ),
+                            gradient: LinearGradient(colors: [
+                              Color(0x36FFFFFF),
+                              Color(0x0FFFFFFF),
+                            ])),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.center,
@@ -157,7 +154,8 @@ class _ImageViewState extends State<ImageView> {
                                   fontSize: 16,
                                   color: Colors.white70,
                                   fontWeight: FontWeight.w500,
-                                ),),
+                                ),
+                              ),
                             ),
                           ],
                         ),
@@ -165,9 +163,9 @@ class _ImageViewState extends State<ImageView> {
                     ],
                   ),
                 ),
-
-                SizedBox(width: 5,),
-
+                SizedBox(
+                  width: 5,
+                ),
                 Container(
                   height: 50,
                   decoration: BoxDecoration(
@@ -177,24 +175,26 @@ class _ImageViewState extends State<ImageView> {
                   ),
                   child: Center(
                     child: IconButton(
-                        onPressed: () {
-                          isDownloaded ? setState(() {
-                            _wallpaper = 'Already Downloaded';
-                          }) : kIsWeb ? _openNewTap() : _save() ;
-                        },
-                        icon: Icon(
-                          Icons.file_download,
-                          color: Colors.white54,
-                          size: 30,
-                        ),
+                      onPressed: () {
+                        isDownloaded
+                            ? setState(() {
+                                _wallpaper = 'Already Downloaded';
+                              })
+                            : kIsWeb
+                                ? _openNewTap()
+                                : _save();
+                      },
+                      icon: Icon(
+                        Icons.file_download,
+                        color: Colors.white54,
+                        size: 30,
+                      ),
                     ),
                   ),
                 )
               ],
             ),
           ),
-
-
           Container(
             height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
@@ -207,15 +207,19 @@ class _ImageViewState extends State<ImageView> {
                   onPressed: () {
                     Navigator.pop(context);
                   },
-                  icon: Icon(Icons.arrow_back_ios,
-                    color: Colors.white,),
+                  icon: Icon(
+                    Icons.arrow_back_ios,
+                    color: Colors.white,
+                  ),
                 ),
                 IconButton(
-                    onPressed: () {
-                      _shareImageLink();
-                    },
-                    icon: Icon(Icons.ios_share,
-                    color: Colors.white,),
+                  onPressed: () {
+                    _shareImageLink();
+                  },
+                  icon: Icon(
+                    Icons.ios_share,
+                    color: Colors.white,
+                  ),
                 ),
               ],
             ),
@@ -226,67 +230,62 @@ class _ImageViewState extends State<ImageView> {
   }
 
   _shareImageLink() {
-
-    kIsWeb ? Share.share(widget.imgUrlLarge,subject: 'Share Wallpaper Link') : Share.share(widget.imgUrl,subject: 'Share Wallpaper Link');
-
+    kIsWeb
+        ? Share.share(widget.imgUrlLarge, subject: 'Share Wallpaper Link')
+        : Share.share(widget.imgUrl, subject: 'Share Wallpaper Link');
   }
 
   _save() async {
-
     setState(() {
       _wallpaper = 'Downloading...';
     });
 
-    if(Platform.isAndroid || Platform.isIOS){
+    if (Platform.isAndroid || Platform.isIOS) {
       await _askPermission();
     }
-    var response = await Dio().get(
-      widget.imgUrl,
-      options: Options(responseType: ResponseType.bytes)
-    );
-    final result = await ImageGallerySaver.saveImage(Uint8List.fromList(response.data));
+    var response = await Dio()
+        .get(widget.imgUrl, options: Options(responseType: ResponseType.bytes));
+    final result = await ImageGallerySaverPlus.saveImage(
+        Uint8List.fromList(response.data));
 
     print(result);
 
-    if(result['isSuccess'] == false){
+    if (result['isSuccess'] == false) {
       setState(() {
         isDownloaded = false;
         _wallpaper = 'Wallpaper not saved';
       });
-    }
-    else{
+    } else {
       setState(() {
         isDownloaded = true;
         _wallpaper = 'Wallpaper saved';
       });
     }
-
   }
 
   Future<void> _setWallpaper(int wallpaperType) async {
-
-    if(kIsWeb){
+    if (kIsWeb) {
       _showAlertDialog('Desktop');
-    }
-
-    else if(Platform.isAndroid){
+    } else if (Platform.isAndroid) {
       setState(() {
         _wallpaper = 'Setting Wallpaper ...';
       });
 
-      String result;
+      String? result;
 
       var file = await DefaultCacheManager().getSingleFile(widget.imgUrl);
 
       print(file.path);
 
-      try{
-        result = await WallpaperManager.setWallpaperFromFile(file.path, wallpaperType);
+      try {
+        result = await WallpaperManagerPlus().setWallpaper(
+          file.path,
+          wallpaperType,
+        );
 
         setState(() {
           isWallpaperSet = true;
         });
-
       } on PlatformException {
         result = "Wallpaper failed to set.";
         setState(() {
@@ -294,47 +293,40 @@ class _ImageViewState extends State<ImageView> {
         });
       }
 
-      if(!mounted) return;
+      if (!mounted) return;
       setState(() {
-        _wallpaper = result;
+        _wallpaper = result ?? 'Wallpaper not set';
       });
-    }
-
-    else if(Platform.isIOS){
+    } else if (Platform.isIOS) {
       _showAlertDialog('iOS');
     }
-
   }
 
   _showAlertDialog(String query) {
-
     CupertinoAlertDialog alertDialog = CupertinoAlertDialog(
       title: Text('Wallpaper not set'),
-      content: Text('You can not set wallpaper for $query. Download the image and set wallpaper manually.'),
+      content: Text(
+          'You can not set wallpaper for $query. Download the image and set wallpaper manually.'),
     );
 
     showDialog(
         context: context,
-        builder: (BuildContext context){
+        builder: (BuildContext context) {
           return alertDialog;
-        }
-    );
+        });
   }
 
   _openNewTap() async {
-
-    if(await canLaunch(widget.imgUrlLarge)){
+    if (await canLaunch(widget.imgUrlLarge)) {
       await launch(
         widget.imgUrlLarge,
         forceSafariVC: true,
         forceWebView: true,
         enableJavaScript: true,
       );
-    }
-    else {
+    } else {
       throw 'Could not lunch ${widget.imgUrlLarge}';
     }
-
   }
 
   _askPermission() async {
@@ -343,16 +335,11 @@ class _ImageViewState extends State<ImageView> {
         Permission.photos,
       ].request();
       print(permissions[Permission.photos]);
-
     } else {
-
       Map<Permission, PermissionStatus> permissions = await [
         Permission.storage,
       ].request();
       print(permissions[Permission.storage]);
-
     }
   }
-
 }
-
